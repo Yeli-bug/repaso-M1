d820a3ac4771b034dabfd29330b95fc429e06626
jmella22 <ing.jose.mella@gmail.com>
2022-05-10 18:45:02 -0400

    ejercicios basicos

--- ejercicios.js
+++ ejercicios.js
@@ -56,3 +56,91 @@ function validAnagram(first, second) {}
  */
 
 function sumZero(arr) {}
+
+// ------------------EJERCICIO V------------------------------//
+/**
+ * Escribe una funcion que reciba un array y un numero promedio. 
+ * Determina si dentro del array dos numeros dan el promedio pasado.
+ * Deberia retornar true si dos numeros sumados dentro del array 
+ * es igual al num pasado.
+
+ * @param {Array} arr1 - esto es un array.
+ * @returns {Array || undefined} - deberia retornar un array o undefined.
+ */
+
+function averagePair(arr, num) {}
+
+// ------------------EJERCICIO VI------------------------------//
+/**
+ * Escribe una funcion llamada recursiveRange que reciba un numero y 
+ * sume todos los numeros desde 0 hasta el numero dado.
+ * Debe retornar esa suma.
+
+ * recursiveRange(6) // 0 + 1 + 2 + 3 + 4 + 5 + 6 = 21
+
+ * @param {Array} arr1 - esto es un array.
+ * @returns {Array || undefined} - deberia retornar un array o undefined.
+ */
+
+function recursiveRange(num) {}
+
+// ------------------EJERCICIO VII------------------------------//
+/**
+ * Escribe una funcion llamada productOfArray que reciba un array y 
+ * mutiplique cada elemento con el siguiente.
+ * Debe retornar el total de la multiplicacion de todos los elementos.
+
+ * @param {Array} arr - esto es un array.
+ * @returns {Number} - deberia retornar un number.
+ */
+
+function productOfArray(arr) {}
+
+// ------------------EJERCICIO VIII------------------------------//
+/**
+ * Escribe una funcion llamada factorial. Debe retornar el factorial 
+ * de un numero.
+ * Factoriar es cuando multiplicamos un numeros por todos los que estan 
+ * debajo de el.
+
+ * @param {Number} num - esto es number.
+ * @returns {Number} - deberia retornar un number.
+ */
+
+function factorial(num) {}
+
+// ------------------EJERCICIO IX------------------------------//
+/**
+ * Escribe una funcion llamada powers que reciba una base y un exponente.
+ * Debera retornar la base por el exponente. Similar a como funciona 
+ * Math.pow();
+ * No deberia fijarme los casos negativos
+
+ * @param {Array} arr1 - esto es un array.
+ * @returns {Array || undefined} - deberia retornar un array o undefined.
+ */
+
+function power(base, exp) {}
+
+// ------------------EJERCICIO X------------------------------//
+/**
+ * Escribe una funcion llamada reverse que acepte un string.
+ * Debe retornar el string a la inversa.
+
+ * @param {String} str - esto es un string.
+ * @returns {String} - deberia retornar un string.
+ */
+
+function reverse(str) {}
+
+// ------------------EJERCICIO XI------------------------------//
+/**
+ * Escribe una funcion llamada isPalindrome que reciba un string.
+ * Debe retornar true si la string es un palindromo.
+ * Osea si se lee igual de derecha a izquierda, como neuquen.
+
+ * @param {String} str - esto es un string.
+ * @returns {String} - deberia retornar un string.
+ */
+
+function isPalindrome(str) {}
